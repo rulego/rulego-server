4720b8285145672fbf6e8f7521fd350f3ded7322
rulego-team <rulego@outlook.com>
2026-03-16 10:48:08 +0800

    feat:增加串口通信节点

--- cmd/server/with_iot.go
+++ cmd/server/with_iot.go
@@ -1,5 +1,3 @@
-//go:build with_iot
-
 package main
 
 import (
@@ -8,4 +6,19 @@ import (
 	_ "github.com/rulego/rulego-components-iot/endpoint/opcua"
 	_ "github.com/rulego/rulego-components-iot/external/modbus"
 	_ "github.com/rulego/rulego-components-iot/external/opcua"
+	"github.com/rulego/rulego-components-iot/external/serial"
 )
+
+func init() {
+	// 获取串口列表函数，支持实时获取
+	getSerialPorts := func() interface{} {
+		serialPortsList, _ := serial.GetPortsList()
+		return map[string]interface{}{
+			"port": serialPortsList,
+		}
+	}
+	service.RegisterBuiltin("x/serialIn", getSerialPorts)
+	service.RegisterBuiltin("x/serialOut", getSerialPorts)
+	service.RegisterBuiltin("x/serialRequest", getSerialPorts)
+	service.RegisterBuiltin("x/serialControl", getSerialPorts)
+}
